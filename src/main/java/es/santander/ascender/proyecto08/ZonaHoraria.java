3e9bce4f8378350040e443071005e581a5c2f46d
elsa <elsa@gmail.com>
2024-11-19 14:08:32 +0100

    Clase zonahoraria

---
+++ src/main/java/es/santander/ascender/proyecto08/ZonaHoraria.java
@@ -0,0 +1,5 @@
+package es.santander.ascender.proyecto08proyecto08;
+
+public class ZonaHoraria {
+
+}
---
+++ src/test/java/es/santander/ascender/proyecto08/ZonaHorariaTest.java
@@ -0,0 +1,5 @@
+package es.santander.ascender.proyecto08;
+
+public class ZonaHorariaTest {
+
+}
