6c769a19fc87dda8fad2dc7893c1822fac69d7f8
curso12 <curso12@curso12>
2024-11-20 10:58:09 +0100

    Time Zone Kyiv

---
+++ src/main/java/es/santander/ascender/proyecto08/DateTimeWithTimeZone.java
@@ -0,0 +1,8 @@
+package es.santander.ascender.proyecto08;
+
+public interface DateTimeWithTimeZone {
+    public abstract String getActual();
+    public abstract int getDayOfTheWeek();
+    
+
+}
--- src/main/java/es/santander/ascender/proyecto08/ZonaHoraria.java
+++ src/main/java/es/santander/ascender/proyecto08/ZonaHoraria.java
@@ -1,5 +1,20 @@
-package es.santander.ascender.proyecto08proyecto08;
+package es.santander.ascender.proyecto08;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
+public class ZonaHoraria {
+    public String formatDate(Date dateToFormat, String timezoneId) {
+        SimpleDateFormat formatter = new SimpleDateFormat("YY/MM/dd HH:mm:ss");
+        TimeZone timezone = TimeZone.getTimeZone(timezoneId);
+        formatter.setTimeZone(timezone);
+
+        return formatter.format(dateToFormat);
+        
+
+    }
+
+}
 public class ZonaHoraria {
 
 }
